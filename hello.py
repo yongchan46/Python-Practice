bdf48398d2633d72ee23a3af4651688e84283aa6
조용찬 <101387953+dyongchan@users.noreply.github.com>
2022-03-23 22:54:56 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,34 +1,8 @@
 # 조용찬 202004484 컴퓨터전자시스템전공
 a = "Python programming"
 b = "Funny Python"
-
+c = 3.57
 
 print(a)
 print(b)
-
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-grades = [67, 100, 87, 56, 99, 100, 84, 76, 83, 55, 99]
-
-
-'''
->>> 5 / 0
-Traceback (most recent call last):
-  File "<pyshell#109>", line 1, in <module>
-    5 / 0
-ZeroDivisionError: division by zero
-'''
-
->>> type(1)
-<class 'int'>
-
->>> type(15)
-<class 'int'>
-
->>> type(0)
-<class 'int'>
-
->>> type(-46)
-<class 'int'>
-  
-  
+print(c)
